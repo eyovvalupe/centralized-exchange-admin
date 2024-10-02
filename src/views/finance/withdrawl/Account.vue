e6b6c7560f8a9d28d389826351da4883424242a2
xiaoyu22333 <xiaoyu@uuiden.com>
2024-10-02 17:46:45 +0800

    提现

--- src/views/finance/withdrawl/Account.vue
+++ src/views/finance/withdrawl/Account.vue
@@ -1,16 +1,24 @@
 <template>
    <el-dialog :close-on-click-modal="false" width="540" class="reset-el-styte" title="提现账号" v-model="show" :append-to-body="true"
-    @close="emit('close', false)">
+    @close="emit('close', false)"
+    >
     <div class="p-2">
-      <div>地址：地址 地址地址</div>
-      <div class="address-code">
-        <qrcode-vue :value="link" :size="188" level="H" />
+      <div v-if="channel === 'crypto'">
+        <div>钱包地址：{{address}}</div>
+        <div class="address-code">
+          <qrcode-vue :value="link" :size="188" level="H" />
+        </div>
+      </div>
+      <div v-else-if="channel === 'bank'">
+        <div>姓名：{{account_name}}</div>
+        <div>银行名：{{bank_name}}</div>
+        <div>银行卡号：{{bank_card_number}}</div>
       </div>
     </div>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="emit('close', false)">取消</el-button>
-        <el-button type="primary" class="default_btn" @click="handleSubmit" :loading="isLoading">确定 </el-button>
+        <el-button type="primary" class="default_btn" @click="emit('close', false)" :loading="isLoading">确定 </el-button>
       </span>
     </template>
   </el-dialog>
@@ -22,6 +30,7 @@ import { ref, reactive, onMounted } from 'vue'
 import { ElMessage } from 'element-plus'
 import { getSessionToken } from '/@/api/modules/base.api'
 import QrcodeVue from 'qrcode.vue'
+// import { tr } from 'element-plus/es/locale'
 
 const props = defineProps({
   order_no: { // 行数据
@@ -35,7 +44,12 @@ const formLabelWidth = ref(100)
 const loading = ref(false)
 const isLoading = ref(false)
 const show = ref(true)
-const link = ref('')
+const address = ref('')
+const channel = ref('')
+const account_name = ref('')
+const bank_name = ref('')
+const bank_card_number = ref('')
+
 const form = reactive({
   order_no: '',
   status: '',
@@ -55,8 +69,14 @@ onMounted(() => {
 const trigger = ['blur', 'change']
 const init=()=>{
   apiWithdrawAccount({order_no:props.order_no}).then(res=>{
-    console.log(res);
-    link.value = res
+    if(res){
+      address.value = res.address
+      account_name.value = res.account_name
+      bank_name.value = res.bank_name
+      bank_card_number.value = res.bank_card_number
+      channel.value = res.channel
+    }
+    // link.value = res
   })
 }
 const rules = {
@@ -65,32 +85,5 @@ const rules = {
 }
 
 const emit = defineEmits(['close', 'success'])
-// 新增
-const handleSubmit = async () => {
-  ruleForm.value.validate(valid => {
-    if (valid) {
-      submit()
-    }
-  })
-}
 
-const submit = async () => {
-  // 发送请求
-  isLoading.value = true
-  try {
-    const token = await getSessionToken()
-    const send = { ...form, token };
-    const result = await apiDepositCheck(send)
-    console.log(result)
-    ElMessage({
-      type: 'tips',
-      message: '操作成功',
-      offset: 200
-    })
-    isLoading.value = false
-    emit('close', { reload: true })
-  } catch (error) {
-    isLoading.value = false
-  }
-}
 </script>
