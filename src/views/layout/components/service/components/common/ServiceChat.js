ee2d81fd453f968ed61cc9b381276beb63e7e9c9
eyovvalupe <rylantwcn705172@hotmail.com>
2024-11-06 18:18:50 +0800

    fix:修正向用户发送消息

--- src/views/layout/components/service/components/common/ServiceChat.js
+++ src/views/layout/components/service/components/common/ServiceChat.js
@@ -1,12 +1,40 @@
-
 import { useServiceStore } from '/@/store'
 import io from 'socket.io-client'
 import { ElNotification } from 'element-plus'
-const audio = new Audio('/tips.mp3');
+const audio = new Audio('/tips.mp3')
 class Service {
   constructor() {
-    this.socketNum = null;
+    this.socket = null
+    this.socketNum = null
     this.isConnected = false
+  }
+  init() {
+    if (!this.socket) {
+      const token = localStorage.getItem('token') || useUserStore().token
+      const URL = import.meta.env.VITE_PROXY_WEBSOCKET2 + '/liveSupport'
+      this.socket = io.connect(URL, {
+        transports: ['websocket'],
+        reconnectionDelayMax: 10000,
+        query: {
+          auth: token,
+        },
+      })
+      this.socket.on('connect', () => {
+        this.isConnected = true
+        console.log('连接成功')
+      })
+
+      this.socket.on('disconnect', () => {
+        console.log('连接断开')
+        this.isConnected = false
+      })
+      this.socket.on('receive', message => {
+        const useService = useServiceStore()
+        message.data.forEach(data => {
+          useService.pushNewMessageList(data)
+        });
+      })
+    }
   }
   initNum() {
     if (!this.socketNum) {
@@ -17,8 +45,8 @@ class Service {
         transports: ['websocket'],
         reconnectionDelayMax: 10000,
         query: {
-          auth: token
-        }
+          auth: token,
+        },
       })
       this.socketNum.on('connect', () => {
         console.log('连接成功socketNum')
@@ -27,19 +55,20 @@ class Service {
         console.log('连接断开')
       })
       this.socketNum.on('receive', message => {
-        const { channel, type, num } = message.data;
+        console.log(message)
+        const { channel, type, num } = message.data
         let msg = ''
-        if(channel === 'deposit'){
+        if (channel === 'deposit') {
           msg = '您有1条新的充值消息'
-        }else if(channel === 'support'){
+        } else if (channel === 'support') {
           msg = '您有1条新的客服消息'
-        }else if(channel === 'withdraw'){
+        } else if (channel === 'withdraw') {
           msg = '您有1条新的提现消息'
-        }else if(channel === 'kyc'){
+        } else if (channel === 'kyc') {
           msg = '您有1条新的实名审核消息'
         }
         if (type === 'new' && useService.playVoice) {
-          audio.play();
+          audio.play()
           ElNotification({
             title: '新消息提醒！',
             message: msg,
@@ -50,30 +79,35 @@ class Service {
         useService.setMessageNumObj(channel, num)
       })
     }
-    return this.socketNum
+    // return this.socketNum
   }
   // 发送消息
   sendMessage(type, message) {
-    console.log(message);
+    console.log(message)
     if (!this.socket) {
-      console.warn('Socket is not initialized.');
-      return;
+      console.warn('Socket is not initialized.')
+      return
     }
     try {
-      this.socket.emit(type, JSON.stringify(message));
+      this.socket.emit(type, JSON.stringify(message))
     } catch (e) {
-      console.error('failure to send message', e);
-      throw e;
+      console.error('failure to send message', e)
+      throw e
     }
   }
 
   // 断开
   destroy() {
-    this.isConnected = false;
+    this.isConnected = false
+    if (this.socket) {
+      this.socket.off() // 移除所有监听器
+      this.socket.disconnect()
+      this.socket = null
+    }
     if (this.socketNum) {
-      this.socketNum.off(); // 移除所有监听器
-      this.socketNum.disconnect();
-      this.socketNum = null;
+      this.socketNum.off() // 移除所有监听器
+      this.socketNum.disconnect()
+      this.socketNum = null
     }
   }
 }
--- src/views/layout/components/service/components/common/sendMessage.vue
+++ src/views/layout/components/service/components/common/sendMessage.vue
@@ -1,15 +1,21 @@
 <template>
   <footer class="send-box h-[125px] border-t-[1px] border-solid border-[#E6E6E6] flex items-center justify-between">
-    <div ref="inputDom" contenteditable="true" class="border-0 h-full w-full inputDom" placeholder="请输入消息"
-        style="white-space: nowrap; overflow-x: auto" @keyup.enter="sendchat()"></div>
-      <div class="send-box-icon flex items-center justify-between">
-        <img :src="sendMessage" alt="" class="cursor-pointer" @click="sendchat()">
-        <label>
-          <img :src="sendImg" alt="" class="mx-8 cursor-pointer">
-          <input type="file" style="display: none" ref="fileInput" @change="uploadImg">
-        </label>
-        <img :src="moreImg" @click="setDelel" class="ml-4 mr-4 cursor-pointer">
-      </div>
+    <div
+      ref="inputDom"
+      contenteditable="true"
+      class="border-0 h-full w-full inputDom"
+      placeholder="请输入消息"
+      style="white-space: nowrap; overflow-x: auto"
+      @keyup.enter="sendchat()"
+    ></div>
+    <div class="send-box-icon flex items-center justify-between">
+      <img :src="sendMessage" alt="" class="cursor-pointer" @click="sendchat()" />
+      <label>
+        <img :src="sendImg" alt="" class="mx-8 cursor-pointer" />
+        <input type="file" style="display: none" ref="fileInput" @change="uploadImg" />
+      </label>
+      <img :src="moreImg" @click="setDelel" class="ml-4 mr-4 cursor-pointer" />
+    </div>
   </footer>
 </template>
 
@@ -21,27 +27,38 @@ import sendImg from '/@/assets/imgs/service/sendImg.png'
 import { _compressImg, randomFileName } from '/@/utils'
 import { useServiceStore } from '/@/store'
 import { ServiceChat } from './ServiceChat'
+ServiceChat.init()
+ServiceChat.initNum()
 const useService = useServiceStore()
 
 const inputDom = ref(null)
 const sendchat = (type = 'text', msg) => {
   // 发送消息前先移除之前的消息监听器
-  ServiceChat.socket.off('supportReceiveMsg')
-  ServiceChat.sendMessage('send', { type, chatid: useService.chatid, content: msg || inputDom.value.textContent })
+  // ServiceChat.socket.off('supportReceiveMsg')
+  const message = { type, chatid: useService.chatid, content: msg || inputDom.value.textContent }
+  ServiceChat.sendMessage('send', message)
+  // useService.pushNewMessageList({ ...message, time: Date.now(), direction: 'receive' })
   inputDom.value.textContent = ''
 }
 
 const fileInput = ref(null)
 const setDelel = () => {
-  useService.setSelectMessageStatus(!useService.isSelectMessage);
+  useService.setSelectMessageStatus(!useService.isSelectMessage)
 }
-const uploadImg = (event) => {
+const uploadImg = event => {
   const file = event.target.files[0]
-  const fileName = randomFileName(file.name);
-  const tmp_arr = { chatid: useService.chatid, content: '图片加载中...', fileName, time: Date.now(), direction: 'receive', type: "img" }
-  useService.pushNewMessageList(tmp_arr)
-  const apiUrl = `${import.meta.env.VITE_UPLOAD_ADDRESS}${fileName}`
-  const reader = new FileReader();
+  const fileName = randomFileName(file.name)
+  const tmp_arr = {
+    chatid: useService.chatid,
+    content: '图片加载中...',
+    fileName,
+    time: Date.now(),
+    direction: 'receive',
+    type: 'img',
+  }
+  // useService.pushNewMessageList(tmp_arr)
+  const apiUrl = `${import.meta.env.VITE_UPLOAD_ADDRESS2}${fileName}`
+  const reader = new FileReader()
   reader.onloadend = function () {
     let ratio = 0.5
     const size = file.size / 1024
@@ -52,40 +69,39 @@ const uploadImg = (event) => {
     } else {
       ratio = 0.5
     }
-    _compressImg(reader.result, ratio, (base64Img) => {
-      const base64result = base64Img.substr(base64Img.indexOf(',') + 1);
+    _compressImg(reader.result, ratio, base64Img => {
+      const base64result = base64Img.substr(base64Img.indexOf(',') + 1)
       fetch(`${apiUrl}`, {
         method: 'PUT',
         headers: {
-          'Authorization': import.meta.env.VITE_UPLOAD_TOKEN,
-          'Content-Type': 'application/json'
+          Authorization: import.meta.env.VITE_UPLOAD_TOKEN2,
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           message: useService.chatid,
           content: base64result,
-          branch: 'main'
-        })
+          branch: 'main',
+        }),
       })
         .then(response => response.json())
         .then(data => {
           if (data.content && data.content.download_url) {
             sendchat('img', data.content.download_url)
           } else {
-            console.error('上传失败:', data);
+            console.error('上传失败:', data)
           }
         })
         .catch(error => {
           console.error('上传出错:', error)
-        });
+        })
     })
   }
-  reader.readAsDataURL(file);
+  reader.readAsDataURL(file)
 }
-
 </script>
 
 <style lang="scss" scoped>
-[contenteditable="true"]:focus {
+[contenteditable='true']:focus {
   outline: none;
 }
 
