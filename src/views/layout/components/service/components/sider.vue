c6d5471bdad09a835eb8ee5203f96f2d73f1e81b
eyovvalupe <rylantwcn705172@hotmail.com>
2024-11-09 16:59:57 +0800

    feat:修改年度显示

--- src/views/layout/components/service/components/sider.vue
+++ src/views/layout/components/service/components/sider.vue
@@ -170,11 +170,12 @@ const transferTime = time => {
     const dayOfWeek = date.getDay()
     return weekday[dayOfWeek]
   } else {
+    const year = String(date.getFullYear()).slice(-4)
     const month = (date.getMonth() + 1).toString().padStart(2, '0')
     const day = date.getDate().toString().padStart(2, '0')
     const hours = date.getHours().toString().padStart(2, '0')
     const minutes = date.getMinutes().toString().padStart(2, '0')
-    return `${month}-${day} ${hours}:${minutes}`
+    return `${year}-${month}-${day} ${hours}:${minutes}`
   }
 }
 const setRemark = item => {
